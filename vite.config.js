6097ffc6b0f5104858ce69b411b9ac95955d0999
INFOInstituteofEngineering <info.engg.socialmedia@gmail.com>
2026-01-21 12:29:29 +0530

    clear bug 4

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Infest2k25-production/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
